2dee0f06bd3589ba518c0af381727125f24d0d3b
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-12 20:28:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
+require 'pry'
 require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -12,7 +14,16 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
 
-  end
+    user = User.find_by(:username => params[:username])
+
+    if user
+      session[:user_id] = user.id
+      redirect to '/account'
+    else 
+      erb :error 
+    end 
+  end 
+    
 
   get '/account' do
 
